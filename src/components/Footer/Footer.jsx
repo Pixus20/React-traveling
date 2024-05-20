d53860496492ec998374f9e35c04665efed93e7a
Pixus20 <mihalcukvasa01@gmail.com>
2024-05-20 20:47:27 +0300

    create footer full

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import './footer.css';
 
 import videoFooter from '../../mediaFiles/PlaneBg.mp4';
 
+import { FaChevronRight, FaInstagram, FaTripadvisor, FaTwitter, FaYoutube } from "react-icons/fa";
 import { FiSend } from "react-icons/fi";
+import { MdTravelExplore } from "react-icons/md";
 
 const Footer = () => {
    return (
@@ -25,7 +27,113 @@ const Footer = () => {
                   </button>
                </div>
             </div>
+            <div className="footerCard flex">
+               <div className="footerIntro flex">
+                  <div className="logoDiv">
+                     <a href="#" className='logo flex'>
+                     <MdTravelExplore  className='icon'/>Travel
+                     </a>
+                  </div>
+                  <div className="footerParagraph">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                     Animi repellendus necessitatibus beatae mollitia nobis sit veritatis iusto, quam, 
+                     debitis quis optio quidem quasi tenetur dolore perspiciatis sequi cupiditate at similique.
+                  </div>
+                  <div className="footerSocials flex">
+                  <FaTwitter className='icon'/>
+                  <FaYoutube className='icon'/>
+                  <FaInstagram  className='icon'/>
+                  <FaTripadvisor className='icon'/>
+
+                  </div>
+               </div>
+               <div className="footerLinks grid">
+                  {/* Group One */}
+                  <div className="linkGroup">
+                     <span className="groupTitle">
+                        OUR AGENCY
+                     </span>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Services
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Insurance
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Agency
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Tourism
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Payment
+                     </li>
+                  </div>
+                  {/* Group two */}
+                  <div className="linkGroup">
+                     <span className="groupTitle">
+                        PARTNERS
+                     </span>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Bookings
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Rentcars
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        HostelWorld
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Trivago
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        TripAdvisor
+                     </li>
+                  </div>
+                  {/* Group three */}
+                  <div className="linkGroup">
+                     <span className="groupTitle">
+                        LAST MINUTE
+                     </span>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        London
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        California
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Indonesia
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Europe
+                     </li>
+                     <li className="footerList flex">
+                        <FaChevronRight  className='icon'/>
+                        Oceania
+                     </li>
+                  </div>
+               </div>
+               <div className="footerDiv flex">
+                  <small>BEST TRAVEL WEBSITE THEME</small>
+                  <small>COPYRIGHTS RESERVED - ISRATECH 2022</small>
+               </div>
+            </div>
          </div>
+         
       </section>
    )
 }
